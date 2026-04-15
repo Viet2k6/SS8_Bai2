20d3ba1cdaab597d3e0c75a06c99310a201ccb0f
Đăng Việt <trandangviet00@gmail.com>
2026-04-15 21:18:53 +0700

    upload bai 2

--- src/main/java/org/example/ss8_bai2/config/AppConfig.java
+++ src/main/java/org/example/ss8_bai2/config/AppConfig.java
@@ -4,37 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
-import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "org.example.ss8_bai2")
-public class AppConfig implements WebMvcConfigurer {
+public class AppConfig {
     @Bean
-    public SpringResourceTemplateResolver springResourceTemplateResolver() {
-        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
-        springResourceTemplateResolver.setPrefix("/WEB-INF/views/");
-        springResourceTemplateResolver.setSuffix(".html");
-        springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
-        springResourceTemplateResolver.setCharacterEncoding("UTF-8");
-        return springResourceTemplateResolver;
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
     }
 
     @Bean
-    public SpringTemplateEngine springTemplateEngine() {
-        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
-        springTemplateEngine.setTemplateResolver(springResourceTemplateResolver());
-        return springTemplateEngine;
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+        return engine;
     }
 
     @Bean
-    public ThymeleafViewResolver thymeleafViewResolver() {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(springTemplateEngine());
-        return viewResolver;
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
     }
 }
